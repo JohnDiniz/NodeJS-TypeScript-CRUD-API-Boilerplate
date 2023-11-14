02ca0aeec2fced23a47b90daf6127be14aea9018
João Vitor Diniz Mistrinel <97896243+JohnDiniz@users.noreply.github.com>
2023-11-14 20:18:51 -0300

    add husky

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ import swaggerUI from 'swagger-ui-express';
 
 const app = express();
 
-app.use(express.json());
-app.use('/health', healthRouter);
-app.use('/log', logRouter);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+              app.use(express.json());
+              app.use('/health', healthRouter);
+              app.use('/log', logRouter);
+              app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 export function init(): Promise<Express> {
   connectDb();
